bbee5e5fb5cba98ba9db40d3800a377b40aa5fe5
Milan Weibel <milan.weibel@uc.cl>
2024-06-18 16:03:11 -0400

    dev: second sketch. fix ratio float behavior, update pins, first write of output

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -9,11 +9,12 @@ void setup() {
   // example:
   // pinMode(13, OUTPUT); // Example: Set pin 13 as an OUTPUT
   // Serial.begin(9600);  // Start serial communication at 9600 baud rate
+
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 
-  pinMode(A2, OUTPUT); pinMode(A2, HIGH);
-  pinMode(A3, OUTPUT); pinMode(A3, HIGH);
+  pinMode(2, OUTPUT); pinMode(2, HIGH);
+  pinMode(3, OUTPUT); pinMode(3, HIGH);
 
 }
 
@@ -24,18 +25,23 @@ void loop() {
   // digitalWrite(13, LOW);    // Turn off the LED
   // delay(1000);              // Wait for 1 second
 
-  // Input
-  int result_sensor_0 = 0;
-  int result_sensor_1 = 0;
-  int ratio = 1;
+  // Inits
+  int reading_sensor_0 = 0;
+  int reading_sensor_1 = 1;
+  float ratio = 1.0;
 
-  result_sensor_0 = analogRead(A0);
-  result_sensor_1 = analogRead(A1);
+  // Input
+  reading_sensor_0 = analogRead(A0);
+  reading_sensor_1 = analogRead(A1);
 
   // Process
-  ratio = result_sensor_0 / result_sensor_1;
+  float ratio = float(reading_sensor_0) / reading_sensor_1;
 
-  // Output
-
-
+  // Category binning and output (only ifs since ordered)
+  if (ratio < 0.2) {
+   digitalWrite(2, HIGH);
+  }
+  else {
+    digitalWrite(2, HIGH);
+  }
 }
