7fc56f9c5e0d286b33decdb9762d874a06430cf1
Milan Weibel <milan.weibel@uc.cl>
2024-06-18 17:08:34 -0400

    lint

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -4,16 +4,12 @@
 // Plastic ratios reference values
 // Masoumi et al 2012 halp me pls
 
-
 void setup() {
   // Serial.begin(9600);  // Figure out serial once sensor interface makes sense
-
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-
   pinMode(2, OUTPUT); pinMode(2, HIGH);
   pinMode(3, OUTPUT); pinMode(3, HIGH);
-
 }
 
 void loop() {
@@ -30,7 +26,7 @@ void loop() {
   float ratio = float(reading_sensor_0) / reading_sensor_1;
 
   // Category binning and output (only ifs since ordered)
-  if (ratio < 0.2) {
+  if (ratio < 0.2) {  // Figure out reference ratios once sensors make sense by either sweat or Masoumi et al
     digitalWrite(2, HIGH);
   }
   else {
