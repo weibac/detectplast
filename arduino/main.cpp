ed4e9c0703c17d686c22312d8cc58dc2b5cc019f
Milan Weibel <milan.weibel@uc.cl>
2024-06-18 17:04:46 -0400

    lint

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -2,13 +2,11 @@
 
 // Init constants 
 // Plastic ratios reference values
+// Masoumi et al 2012 halp me pls
 
 
 void setup() {
-  // Set pin modes, initialize serial communication, etc.
-  // example:
-  // pinMode(13, OUTPUT); // Example: Set pin 13 as an OUTPUT
-  // Serial.begin(9600);  // Start serial communication at 9600 baud rate
+  // Serial.begin(9600);  // Figure out serial once sensor interface makes sense
 
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
@@ -19,12 +17,6 @@ void setup() {
 }
 
 void loop() {
-  // example:
-  // digitalWrite(13, HIGH);   // Turn on the LED connected to pin 13
-  // delay(1000);              // Wait for 1 second
-  // digitalWrite(13, LOW);    // Turn off the LED
-  // delay(1000);              // Wait for 1 second
-
   // Inits
   int reading_sensor_0 = 0;
   int reading_sensor_1 = 1;
@@ -39,7 +31,7 @@ void loop() {
 
   // Category binning and output (only ifs since ordered)
   if (ratio < 0.2) {
-   digitalWrite(2, HIGH);
+    digitalWrite(2, HIGH);
   }
   else {
     digitalWrite(2, HIGH);
